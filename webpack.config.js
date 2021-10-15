728a8157f9dbf5fffe1ff3ed2b0d75874c275e3a
Chris Maxwell <chmaxwel@icloud.com>
2021-10-15 14:21:33 +0200

    update to build

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,23 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "docs"),
